9d77a06ff0ac9abe91fd92c5702b19922f4e7c68
zigzzzag <maximnikiforov1991@gmail.com>
2017-07-18 18:38:45 +0700

    First commit

---
+++ app/src/main/java/com/example/chiefacc/chief_acc_andr/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.chiefacc.chief_acc_andr;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
